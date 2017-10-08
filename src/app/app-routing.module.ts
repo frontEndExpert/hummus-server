b4c7d6afc37b71503e50e5dcc998167f9afc0f94
frontEndExpert <aylon.spigel@gmail.com>
2017-10-08 23:32:51 +0700

    copy in

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { BodyComponent } from './body/body.component'
+import { ContactusComponent } from './contactus/contactus.component'
+import { GalleryComponent } from './gallery/gallery.component'
+    
+const routes: Routes = [
+    { path: '', component: BodyComponent },
+    { path: 'contactus', component: ContactusComponent },
+    { path: 'gallery', component: GalleryComponent },
+    { path: '**', redirectTo: '', pathMatch: 'full' },
+];
+
+@NgModule({
+    imports: [        RouterModule.forRoot(routes)    ],
+    exports: [        RouterModule    ],
+    declarations: []
+})
+export class AppRoutingModule { }
